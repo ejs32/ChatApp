396fa4e2b50a51d3d50e0fcc861eefef90b57102
ejs32 <54948638+ejs32@users.noreply.github.com>
2019-11-26 09:12:32 -0500

    Add files via upload

---
+++ landingpage.php
@@ -0,0 +1,138 @@
+﻿<?php
+session_start();
+?>
+<html>
+<head>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="https://cdn.rawgit.com/mervick/emojionearea/master/dist/emojionearea.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
+<script>
+$(document).ready(function(){
+	var nav = ["Home", "About", "Logout"];
+	let ul = $("<ul>");
+	$("body").append(ul);
+	nav.forEach(function(item, index){
+			let ele = $("<a>");
+			//?page   <- GET variable
+			//#page   <-inline link/scroll to
+			//page.php <-relative link to separate page
+			ele.attr("href", "?page="+item+".php");
+			ele.text(item);
+			ul.append($("<li>").append(ele[0]));
+	});
+	fetch_user();
+	function fetch_user()
+	{
+		$.ajax({
+			url:"fetch_user.php",
+			method:"POST",
+			success:function(data){
+			$('#user_details').html(data);
+			}
+		})
+	}
+
+	setInterval(function(){
+		//update_last_activity();
+		//fetch_user();
+		//update_chat_history_data();
+		fetch_group_chat_history();
+	}, 5000);
+	
+	 /*$.ajax({
+			url: "ajax/get.php", 
+			method: "POST", 
+			data: {"type":"login", "username":"bob", "password":"1234"}, 
+			success: function(result){
+					console.log(result);
+					alert(result);
+					result = JSON.parse(result);
+					alert("Status: " + result.status);
+			},
+			fail: function(jqXHR, textStatus){
+				console.log(jqXHR, textStatus);
+			}
+		});*/
+	$(document).on('click', '.ui-button-icon', function(){
+		$('.user_dialog').dialog('destroy').remove();
+		$('#is_active_public_chat_window').val('no');
+	});
+	$('#public_chat_dialog').dialog({
+		autoOpen:false,
+		width:400
+	});
+
+	$('#public_chat').click(function(){
+		$('#public_chat_dialog').dialog('open');
+		$('#is_active_public_chat_window').val('yes');
+		fetch_public_chat_history();
+	});
+
+	$('#send_public_chat').click(function(){
+		var chat_message = $('#public_chat_message').val();
+		var action = 'insert_data';
+		if(chat_message != '')
+		{
+			$.ajax({
+				url:"public_chat.php",
+				method:"POST",
+				data:{chat_message:chat_message, action:action},
+				success:function(data){
+					$('#public_chat_message').val('');
+					$('#public_chat_history').html(data);
+				}
+			})
+		}
+		else
+		{
+			alert('Type something');
+		}
+	});
+
+	function fetch_public_chat_history()
+	{
+		var public_chat_dialog_active = $('#is_active_public_chat_window').val();
+		var action = "fetch_data";
+		if(public_chat_dialog_active == 'yes')
+		{
+			$.ajax({
+				url:"public_chat.php",
+				method:"POST",
+				data:{action:action},
+				success:function(data)
+				{
+					$('#public_chat_history').html(data);
+				}
+			})
+		}
+	}	
+});
+
+</script>
+</head>
+<body>
+Hello there, <?php echo $_SESSION['user']['name'];?><br>
+
+<div class="col-md-2 col-sm-3">
+					<input type="hidden" id="is_active_public_chat_window" value="no" />
+					<button type="button" name="public_chat" id="public_chat" class="btn btn-warning btn-xs">Public Chat</button>
+				</div>
+<div id="public_chat_dialog" title="Public Chat Window">
+	<div id="public_chat_history" style="height:400px; border:1px solid #ccc; overflow-y: scroll; margin-bottom:24px; padding:16px;">
+
+	</div>
+	<div class="form-group">
+	<textarea name="public_chat_message" id="public_chat_message" class="form-control"></textarea>
+	</div>
+	<div class="form-group" align="right">
+		<button type="button" name="send_public_chat" id="send_public_chat" class="btn btn-info">Send</button>
+	</div>
+</div>
+		
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,47 +1,107 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Login</title>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
 <?php
-require('db.php');
+
+//include('database_connection.php');
+
 session_start();
-// If form submitted, insert values into the database.
-if (isset($_POST['username'])){
-        // removes backslashes
- $username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
- $username = mysqli_real_escape_string($con,$username);
- $password = stripslashes($_REQUEST['password']);
- $password = mysqli_real_escape_string($con,$password);
- //Checking is user existing in the database or not
-        $query = "SELECT * FROM `users` WHERE username='$username'
-and password='".md5($password)."'";
- $result = mysqli_query($con,$query) or die(mysql_error());
- $rows = mysqli_num_rows($result);
-        if($rows==1){
-            $_SESSION['username'] = $username;
-            // Redirect user to index.php
-             header("Location: index.php");
-         }
-         else{
-           echo "<div class='form'>
-           <h3>Username/password is incorrect.</h3>
-           </div>";
-         }
-    }
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
-<div class="form">
-<h1>Log In</h1>
-<form action="" method="post" name="login">
-<input type="text" name="username" placeholder="Username" required />
-<input type="password" name="password" placeholder="Password" required />
-<input name="submit" type="submit" value="Login" />
-</form>
-<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
-</div>
-</body>
+<html>  
+    <head>  
+        <title>Chat Application</title>  
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    </head>  
+    <body>  
+        <div class="container">
+			<br />
+			
+			<h3 align="center">Chat Application</h3><br />
+			<br />
+			<div class="panel panel-default">
+  				<div class="panel-heading">Chat Application Login</div>
+				<div class="panel-body">
+
+					<form method="post">
+						<div class="form-group">
+							<label>Enter Username</label>
+							<input type="text" name="username" class="form-control" required />
+						</div>
+						<div class="form-group">
+							<label>Enter Password</label>
+							<input type="password" name="password" class="form-control" required />
+						</div>
+						<div class="form-group">
+							<input type="submit" name="login" class="btn btn-info" value="Login" />
+						</div>
+						<div align="center">
+							<a href="registration.php">Register</a>
+						</div>
+					</form>
+					<br />
+					<br />
+					<br />
+					<br />
+				</div>
+			</div>
+		</div>
+
+    </body>  
 </html>
+<?php
+	if(isset($_POST['username']) && isset($_POST['password'])){
+		$user = $_POST['username'];
+		$pass = $_POST['password'];
+		//do further validation?
+		try{
+			require('database_connection.php');
+			//$username, $password, $host, $database
+			$query = "
+				SELECT * FROM users 
+				WHERE username = :username LIMIT 1
+			";
+			$stmt = $connect->prepare($query);
+			$stmt->execute(array(":username"=>$user));
+			print_r($stmt->errorInfo());
+			$results = $stmt->fetch(PDO::FETCH_ASSOC);
+			echo var_export($results, true);
+			if($results && count($results) > 0){
+				$hash = password_hash($pass, PASSWORD_DEFAULT);
+				if(password_verify($pass, $hash)){
+					echo "Welcome, " . $results["username"];
+					echo "[" . $results["user_id"] . "]";
+					$user = array("id"=> $results['id'],
+								"name"=> $results['username']
+								);
+					$_SESSION['user'] = $user;
+					$_SESSION['user_id'] = $results['user_id'];
+					$_SESSION['username'] = $results['username'];
+					$sub_query = "
+						INSERT INTO login_details 
+						(user_id) 
+						VALUES ('".$results['user_id']."')
+					";
+					$statement = $connect->prepare($sub_query);
+					$statement->execute();
+					$_SESSION['login_details_id'] = $connect->lastInsertId();
+					echo var_export($user, true);
+					echo var_export($_SESSION, true);
+					header("Location: landingpage.php");
+					
+				}
+				else{
+					echo "Invalid password";
+				}
+			}
+			else{
+					echo "Invalid username";
+			}
+		}
+		catch(Exception $e){
+			echo $e->getMessage();
+		}
+	}
+?>
